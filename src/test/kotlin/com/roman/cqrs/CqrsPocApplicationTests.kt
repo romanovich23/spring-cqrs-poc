fa4db6b8c41b59f6ac48ae9add8c44cd7df35ac0
jroman <jroman@io.builders>
2022-08-07 18:48:29 +0200

    First structure using Spring boot
    Add spring boot framework, gradle and kotlin as tools for this project.

---
+++ src/test/kotlin/com/roman/cqrs/CqrsPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roman.cqrs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CqrsPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
